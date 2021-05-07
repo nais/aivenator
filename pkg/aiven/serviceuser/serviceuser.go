9a9c0a9fd2ee77ec397a401e47567b6c7e20e476
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-05-07 20:42:42 +0200

    Remove unused interface methods

--- pkg/aiven/serviceuser/serviceuser.go
+++ pkg/aiven/serviceuser/serviceuser.go
@@ -7,8 +7,6 @@ import (
 
 type Interface interface {
 	Create(project, service string, req aiven.CreateServiceUserRequest) (*aiven.ServiceUser, error)
-	List(project, serviceName string) ([]*aiven.ServiceUser, error)
-	Delete(project, service, user string) error
 }
 
 func NewManager(client Interface) *Manager {
