c7ed22aedbc6b0f7be45ccf9c28576faa929bcf0
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-06-17 12:06:26 +0200

    100 buckets are way too many

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -42,7 +42,7 @@ var (
 		Name:      "aiven_latency",
 		Namespace: Namespace,
 		Help:      "latency in aiven api operations",
-		Buckets:   prometheus.LinearBuckets(0.05, 0.05, 100),
+		Buckets:   []float64{.005, .010, .015, .020, .025, .030, .035, .040, .045, .050, .1, .2, .3, .4, .5, 1, 2, 3, 4, 5, 10, 15, 20},
 	}, []string{LabelAivenOperation, LabelStatus, LabelPool})
 
 	KubernetesResourcesWritten = prometheus.NewCounterVec(prometheus.CounterOpts{
