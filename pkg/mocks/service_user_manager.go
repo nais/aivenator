2c772ffe1bfc35376354bacf3c6dd537cef778b0
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-08-24 14:46:39 +0200

    Updated mocks

--- pkg/mocks/service_user_manager.go
+++ pkg/mocks/service_user_manager.go
@@ -48,3 +48,26 @@ func (_m *ServiceUserManager) Delete(serviceUserName string, projectName string,
 
 	return r0
 }
+
+// Get provides a mock function with given fields: serviceUserName, projectName, serviceName
+func (_m *ServiceUserManager) Get(serviceUserName string, projectName string, serviceName string) (*aiven.ServiceUser, error) {
+	ret := _m.Called(serviceUserName, projectName, serviceName)
+
+	var r0 *aiven.ServiceUser
+	if rf, ok := ret.Get(0).(func(string, string, string) *aiven.ServiceUser); ok {
+		r0 = rf(serviceUserName, projectName, serviceName)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*aiven.ServiceUser)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, string, string) error); ok {
+		r1 = rf(serviceUserName, projectName, serviceName)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
