e64229cdc7e174d79bc92b223096562fb9a1fd38
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-10-21 09:45:47 +0200

    Disable caching in manager client

--- cmd/aivenator/main.go
+++ cmd/aivenator/main.go
@@ -7,9 +7,12 @@ import (
 	"github.com/nais/aivenator/controllers/aiven_application"
 	"github.com/nais/aivenator/controllers/secrets"
 	"github.com/nais/aivenator/pkg/credentials"
+	"k8s.io/client-go/rest"
 	"net/http"
 	"os"
 	"os/signal"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strings"
 	"syscall"
 	"time"
@@ -142,9 +145,12 @@ func main() {
 
 	syncPeriod := viper.GetDuration(SyncPeriod)
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		SyncPeriod:         &syncPeriod,
 		Scheme:             scheme,
+		SyncPeriod:         &syncPeriod,
 		MetricsBindAddress: viper.GetString(MetricsAddress),
+		NewClient: func(_ cache.Cache, config *rest.Config, options client.Options, _ ...client.Object) (client.Client, error) {
+			return client.New(config, options)
+		},
 	})
 
 	if err != nil {
