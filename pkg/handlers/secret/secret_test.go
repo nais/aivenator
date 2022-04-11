5399a6e89ff1ee541bf7aa504b2959fdd2e15bf5
Morten Lied Johansen <morten.lied.johansen@nav.no>
2022-04-11 14:58:24 +0200

    Update metadata on secret instead of overwriting
    Second step towards being able to set ownerReference on retry

--- pkg/handlers/secret/secret_test.go
+++ pkg/handlers/secret/secret_test.go
@@ -2,6 +2,7 @@ package secret
 
 import (
 	"errors"
+	"github.com/nais/aivenator/pkg/utils"
 	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
@@ -13,7 +14,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 
 	"github.com/nais/aivenator/constants"
-	"github.com/nais/aivenator/pkg/utils"
 )
 
 const (
@@ -103,6 +103,34 @@ func TestHandler_Apply(t *testing.T) {
 					assert.Equal(t, replicaSetName, a.secret.GetOwnerReferences()[0].Name)
 					assert.Equal(t, "ReplicaSet", a.secret.GetOwnerReferences()[0].Kind)
 					assert.Equal(t, "apps/v1", a.secret.GetOwnerReferences()[0].APIVersion)
+				},
+			},
+		},
+		{
+			name: "PreexistingSecret",
+			args: args{
+				application: aiven_nais_io_v1.NewAivenApplicationBuilder(applicationName, namespace).
+					WithSpec(aiven_nais_io_v1.AivenApplicationSpec{SecretName: secretName}).
+					Build(),
+				secret: corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            secretName,
+						Namespace:       namespace,
+						Labels:          map[string]string{"pre-existing-label": "pre-existing-label"},
+						Annotations:     map[string]string{"pre-existing-annotation": "pre-existing-annotation"},
+						OwnerReferences: []metav1.OwnerReference{{Name: "pre-existing-owner-reference"}},
+						Finalizers:      []string{"pre-existing-finalizer"},
+					},
+				},
+				assert: func(t *testing.T, a args) {
+					assert.Contains(t, a.secret.Labels, "pre-existing-label", "existing label missing")
+					assert.Contains(t, a.secret.Labels, constants.AppLabel, "new label missing")
+					assert.Contains(t, a.secret.Annotations, "pre-existing-annotation", "existing annotation missing")
+					assert.Contains(t, a.secret.Annotations, nais_io_v1.DeploymentCorrelationIDAnnotation, "new annotation missing")
+					assert.Contains(t, a.secret.Finalizers, "pre-existing-finalizer", "existing finalizer missing")
+
+					assert.NotContains(t, a.secret.OwnerReferences, metav1.OwnerReference{Name: "pre-existing-owner-reference"}, "old ownerReference still there")
+					assert.Contains(t, a.secret.OwnerReferences, a.application.GetOwnerReference(), "new ownerReference missing")
 				},
 			},
 		},
