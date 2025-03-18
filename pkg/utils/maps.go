dd40d08343336b206bab6ea2b6fc9e4119bebd7d
Kyrre Havik <kyrre.havik@nav.no>
2025-03-18 13:49:59 +0100

    upkeep: modernisert kode
    Co-authored-by: carl hedgren <carl.hedgren@nav.no>
    Co-authored-by: x10an14-nav <christian.chavez@nav.no>

--- pkg/utils/maps.go
+++ pkg/utils/maps.go
@@ -1,24 +1,18 @@
 package utils
 
+import "maps"
+
 func MergeStringMap(src, dst map[string]string) map[string]string {
 	target := make(map[string]string)
-	for k, v := range src {
-		target[k] = v
-	}
-	for k, v := range dst {
-		target[k] = v
-	}
+	maps.Copy(target, src)
+	maps.Copy(target, dst)
 	return target
 }
 
 func MergeByteMap(src, dst map[string][]byte) map[string][]byte {
 	target := make(map[string][]byte)
-	for k, v := range src {
-		target[k] = v
-	}
-	for k, v := range dst {
-		target[k] = v
-	}
+	maps.Copy(target, src)
+	maps.Copy(target, dst)
 	return target
 }
 
