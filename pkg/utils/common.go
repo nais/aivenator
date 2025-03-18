ad647987f601f6eec1e73639305f3e4ef80148d9
Kyrre Havik <kyrre.havik@nav.no>
2025-03-18 13:49:44 +0100

    upkeep: fjernet kode ikke i bruk
    Co-authored-by: carl hedgren <carl.hedgren@nav.no>
    Co-authored-by: x10an14-nav <christian.chavez@nav.no>

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -8,23 +8,12 @@ import (
 	"time"
 
 	aiven_nais_io_v1 "github.com/nais/liberator/pkg/apis/aiven.nais.io/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func Expired(expiredAt time.Time) bool {
 	return time.Now().After(expiredAt)
-}
-
-func ParseTimestamp(expiresAt string, errs *[]error) time.Time {
-	expired, err := Parse(expiresAt)
-	if err != nil {
-		*errs = append(*errs, err)
-	}
-	return expired
 }
 
 func Parse(expiresAt string) (time.Time, error) {
@@ -43,24 +32,6 @@ func GetGVK(scheme *runtime.Scheme, obj runtime.Object) (*schema.GroupVersionKin
 		return nil, fmt.Errorf("no kinds registered for %v", obj)
 	}
 	return &kinds[0], nil
-}
-
-func MakeOwnerReference(in client.Object) (metav1.OwnerReference, error) {
-	metaAccessor, err := meta.Accessor(in)
-	if err != nil {
-		return metav1.OwnerReference{}, err
-	}
-	typeAccessor, err := meta.TypeAccessor(in)
-	if err != nil {
-		return metav1.OwnerReference{}, err
-	}
-
-	return metav1.OwnerReference{
-		APIVersion: typeAccessor.GetAPIVersion(),
-		Kind:       typeAccessor.GetKind(),
-		Name:       metaAccessor.GetName(),
-		UID:        metaAccessor.GetUID(),
-	}, nil
 }
 
 func SelectSuffix(access string) string {
